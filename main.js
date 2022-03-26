512d45a780c2d457899eb417163bf7ecad291b7e
Ubuntu <maddie.madlad@gmail.com>
2022-03-26 23:15:11 +0100

    crawler formatting

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import scrape from "./modules/crawler.js"
 import { arraySize } from "./modules/utils.js"
 import process from "./modules/word_processor.js"
 
-let uris = ['https://elixirschool.com/en','https://elixirschool.com/en/lessons/ecto/associations','https://elixirschool.com/en/get_involved','https://elixirschool.com/blog']
+let uris = ['https://www.meblowapaczka.pl/pl/menu/komody-i-szafki-157.html']
 
 scrape(uris, (keyword_candidates) => {
   console.log(keyword_candidates)
