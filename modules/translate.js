9620a3aaeeaefb89377759d3a49722d41039ba78
Ubuntu <maddie.madlad@gmail.com>
2022-03-27 18:18:17 +0200

    translator now translates all text using async

--- modules/translate.js
+++ modules/translate.js
@@ -6,16 +6,29 @@ export const language = {
   polish: "pl"
 }
 
-export default function translate(paragraph, target_lang, callback){
-  axios.post(translation_api+target_lang, {
+let working_lang
+// asynchronous function that translates given text
+function translate(paragraph, callback) {
+  axios.post(translation_api+working_lang, {
     paragraph
   })
   .then(function (response) {
     callback(response.data)
   })
   .catch(function (error) {
-    throw error
-    callback()
+    return callback(error)
+  })
+}
+//public function of this module. it translates all text in an array asynchronously
+export default function translateAll(paragraphs, target_lang, callback){
+  //translate all text in async mode to save time
+  working_lang=target_lang //set this cus async is stupido and cant pass more then 1 arg
+
+  async.map(paragraphs, translate)
+  .then( translation_results => {
+      callback(translation_results)
+  }).catch( err => {
+      throw err
   });
 
 }
