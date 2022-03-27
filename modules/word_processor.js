72d9f5c4112efd769552035b50455c609255b1ec
Ubuntu <maddie.madlad@gmail.com>
2022-03-27 17:27:25 +0200

    word processor now extracts paragraphs

--- modules/word_processor.js
+++ modules/word_processor.js
@@ -2,10 +2,17 @@ import async from 'async'
 import colors from 'colors'
 import { arraySize as sizeof } from './utils.js'
 
-
+let paragraphs = []
 
 export default function process(keyword_candidates, callback){
 
-
-
+  //first extract long paragraphs that can be used for meta desc
+  keyword_candidates.forEach((paragraph_candidate, i) => {
+    if (sizeof(paragraph_candidate.split(" ")) > 15) {
+      //good paragraph
+      paragraphs.push(paragraph_candidate)
+    }
+  })
+  console.log("Processed paragraphs")
+  callback(paragraphs)
 }
