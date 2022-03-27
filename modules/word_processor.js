ce61b72fdf776dcf31b0b1722c09c51eb91481ee
Ubuntu <maddie.madlad@gmail.com>
2022-03-27 17:36:32 +0200

    processor now indexes the raw data

--- modules/word_processor.js
+++ modules/word_processor.js
@@ -2,17 +2,30 @@ import async from 'async'
 import colors from 'colors'
 import { arraySize as sizeof } from './utils.js'
 
-let paragraphs = []
+let index = {
+  paragraphs: [],
+  keyphrases: [],
+  keywords: []
+}
 
 export default function process(keyword_candidates, callback){
 
   //first extract long paragraphs that can be used for meta desc
-  keyword_candidates.forEach((paragraph_candidate, i) => {
-    if (sizeof(paragraph_candidate.split(" ")) > 15) {
+  keyword_candidates.forEach((keyword, i) => {
+    let length = sizeof(keyword.split(" "))
+    if ( length >= 15) {
       //good paragraph
-      paragraphs.push(paragraph_candidate)
+      index.paragraphs.push(keyword)
+    }
+    if (length == 1) {
+      //a keyword
+      index.keywords.push(keyword)
+    }
+    if (length > 1 && length < 15) {
+      //a keyphrase
+      index.keyphrases.push(keyword)
     }
   })
-  console.log("Processed paragraphs")
-  callback(paragraphs)
+  console.log("Data has been indexed")
+  callback(index)
 }
