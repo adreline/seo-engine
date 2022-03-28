b614cc24e1ee4d563945e8448dd68ba04926cac1
adreline <maddie.madlad@gmail.com>
2022-03-28 23:40:24 +0200

    suspended logging and saving to file

--- modules/utils.js
+++ modules/utils.js
@@ -1,3 +1,7 @@
+import fs from "fs"
+import crypto from "crypto"
+import async from "async"
+import colors from "colors"
 
 const html_tag_blacklist = [
                               "nav",
@@ -24,8 +28,37 @@ export function arraySize(ar){
     });
 
   } catch (e) {
-    console.log(e);
-    return null
+    console.log("Size failed")
+    return 0
   }
   return j
+}
+
+export function suspended_log(msg="", line=0, cur=0){
+  if (typeof msg === 'string') {
+    process.stdout.clearLine(0)
+    process.stdout.cursorTo(0)
+    process.stdout.write(msg)
+  }
+}
+
+export function save_page_to_file(page,dir="./"){
+
+  let new_filename=`${dir}${crypto.randomUUID()}.txt`
+  suspended_log(`Exporting to ${new_filename}`.red)
+
+  let new_list=[`${page.link}`]
+  new_list = new_list.concat(page.dictionary.paragraphs)
+  new_list = new_list.concat(page.dictionary.keyphrases)
+  new_list = new_list.concat(page.dictionary.keywords)
+
+  new_list.forEach((str, i) => {
+    try {
+      fs.appendFileSync(new_filename, `${str}\n`)
+    } catch (e) {
+      console.log(e)
+    }
+  });
+
+
 }
