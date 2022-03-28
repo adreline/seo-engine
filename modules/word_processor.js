4543409540623cf0b12b3c330f3bd182fb358d3d
adreline <maddie.madlad@gmail.com>
2022-03-28 23:40:59 +0200

    processor no longer uses ai module

--- modules/word_processor.js
+++ modules/word_processor.js
@@ -1,23 +1,17 @@
 import async from 'async'
 import colors from 'colors'
-import { arraySize as sizeof } from './utils.js'
-import { write as writeMetaDesc, recap as writeMetaTitle } from './ai_writer.js'
-import translateAll from './translate.js'
-import { language as languages } from './translate.js'
+import { arraySize as sizeof, suspended_log } from './utils.js'
 
-let index = {
-  paragraphs: [],
-  keyphrases: [],
-  keywords: []
-}
-let ai_proposal = {
-  meta_title: "",
-  meta_desc: ""
-}
-export default function process(keyword_candidates, callback){
 
+
+export default function process(dictionary){
+  let index = {
+    paragraphs: [],
+    keyphrases: [],
+    keywords: []
+  }
   //first extract long paragraphs that can be used for meta desc
-  keyword_candidates.forEach((keyword, i) => {
+  dictionary.forEach((keyword, i) => {
     let length = sizeof(keyword.split(" "))
     if ( length >= 15) {
       //good paragraph
@@ -33,30 +27,5 @@ export default function process(keyword_candidates, callback){
     }
   })
 
-  console.log("Data has been indexed")
-  console.log(`Extracted ${sizeof(index.paragraphs)} paragraphs`.yellow)
-  console.log(`Extracted ${sizeof(index.keyphrases)} keyphrases`.yellow)
-  console.log(`Extracted ${sizeof(index.keywords)} keywords`.yellow)
-  //make head section proposal
-  //first translate paragraphs into english
-  translateAll(index.paragraphs,languages.english,(translation_results)=>{
-    //translation is done, use ai to write meta desc and meta title
-    console.log("Paragraphs translated")
-    let article = "" //concat all paragraphs to make title summary
-    translation_results.forEach((item, i) => {
-      article+=item
-    });
-    
-    //call the ai
-    writeMetaTitle({paragraph: article},(result)=>{
-      console.log(`AI title proposal: ${result}`.yellow)
-      writeMetaDesc({prompt: article}, (result)=>{
-        console.log(`AI description proposal: ${result}`.yellow)
-          callback()
-      })
-    })
-    //callback()
-  })
-
-
+  return index
 }
