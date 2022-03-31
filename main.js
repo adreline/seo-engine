50bbd7b6d31e169c7b05d490ea23b828a19af909
Ubuntu <maddie.madlad@gmail.com>
2022-03-31 23:17:08 +0200

    Ditched fs.watch in favour of http server

--- main.js
+++ main.js
@@ -6,52 +6,55 @@ import child_process from "child_process"
 import { arraySize as sizeof, suspended_log, save_page_to_file as export_page } from "./modules/utils.js"
 import process from "./modules/word_processor.js"
 import { write as writeMetaDesc, recap as writeMetaTitle } from './modules/ai_writer.js'
+import http from "http"
+import express from "express"
+import bodyParser from "body-parser"
 
-let suspended_flag=false
-let uris = []
-console.log("Waiting for files".green)
-fs.watch('./temp/tasks',(eventtype, filename)=>{
-  child_process.execSync("sleep 0.5")
-  if (!suspended_flag && eventtype=='change') {
-    suspended_flag=true
-    console.log('File "' + filename + '" detected: ' + eventtype)
-    let file_contents
-    try {
-      file_contents=fs.readFileSync(`./temp/tasks/${filename}`, "utf8")
-      fs.unlinkSync(`./temp/tasks/${filename}`)
-    } catch (e) {
-    console.log(e)
-    return null
-    }
-    uris = file_contents.split("\n")
-    uris.forEach((item, i) => {
-      console.log(item)
-    });
 
-    /*
-    console.log(`Targeting ${sizeof(uris)} pages`.red)
-    scrape(uris, (text_data_collection) => {
-      let total=0
-      text_data_collection.forEach((page, i) => {
-        total+=sizeof(page.dictionary)
-        suspended_log(`Collected ${total} data points`.yellow)
-        page.dictionary = process(page.dictionary)
-      })
-      console.log("\nExporting data to text files".yellow);
-      text_data_collection.forEach((page, i) => {
-        export_page(page,'./temp/output/')
-      })
-      console.log("")
-      suspended_flag=false
+function main(uris, callback){
+  console.log(`Targeting ${sizeof(uris)} pages`.red)
+  scrape(uris, (text_data_collection) => {
+    let total=0
+    text_data_collection.forEach((page, i) => {
+      total+=sizeof(page.dictionary)
+      suspended_log(`Collected ${total} data points`.yellow)
+      page.dictionary = process(page.dictionary)
     })
-    */
-    suspended_flag=false
-  }
+    console.log("\nExporting data to text files".yellow);
+    text_data_collection.forEach((page, i) => {
+      export_page(page,'./temp/output/')
+    })
+    console.log("Done".yellow)
+    callback()
+  })
+}
 
+//-------------SERVER SECTION------------
+const app = express()
+app.set('views', './html')
+app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Handling GET / request
+app.get("/", (req, res, next) => {
+    res.render('index')
 })
-/*
+// Handling post /scrape request
+app.post("/scrape", (req, res, next) => {
+    let uris = req.body.urls.split("\n")
+    main(uris,()=>{
+      res.render('index')
+    })
+})
+// Server setup
+app.listen(5001, () => {
+    console.log("Server is Running".green)
+})
 
-*/
+
+
+
+
 
 /*
 //make head section proposal
